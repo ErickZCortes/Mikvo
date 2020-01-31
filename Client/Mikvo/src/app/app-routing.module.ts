9fb51a81b12916fdc0903b42fb9714976b236f24
ErickZCortes <erickccortes@gmail.com>
2020-01-30 21:21:35 -0600

    login,registro,404,recuperacion

--- Client/Mikvo/src/app/app-routing.module.ts
+++ Client/Mikvo/src/app/app-routing.module.ts
@@ -5,11 +5,18 @@ import { DefaultComponent } from './mikvo/dashboard/layouts/default/default.comp
 import { MainComponent } from './mikvo/dashboard/modules/main/main.component';
 import { UserprofileComponent } from './mikvo/dashboard/modules/userprofile/userprofile.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './mikvo/login/login.component';
+import { RegisterComponent } from './mikvo/register/register.component';
+import { ResetPasswordComponent } from './mikvo/reset-password/reset-password.component';
 
 
 const routes: Routes = [
   { path: 'mikvo', component: MikvoComponent},
   { path: '', redirectTo: 'mikvo', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'reset-password', component: ResetPasswordComponent},
+
   { path:'', component: DefaultComponent,
     children:[
       { path:'dashboard', component: MainComponent },
--- Client/Mikvo/src/app/mikvo/mikvo.module.ts
+++ Client/Mikvo/src/app/mikvo/mikvo.module.ts
@@ -4,10 +4,13 @@ import { MikvoComponent } from './mikvo.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { DefaultModule } from './dashboard/layouts/default/default.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 
 @NgModule({
-  declarations: [MikvoComponent],
+  declarations: [MikvoComponent, LoginComponent, RegisterComponent, ResetPasswordComponent],
   imports: [
     CommonModule,
     HttpClientModule,
